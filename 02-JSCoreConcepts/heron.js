b4720f380ceeaa8d0fe001bea478e8c123ed3ca3
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-03-03 15:47:04 +0100

    Heron's formula

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -1,11 +1,14 @@
 // boki trójkąta
-//a = 3;
-b = 4;
-c = 5;
-h = 7
+let a = 3;
+let b = 4;
+let c = 5;
+// h = 7;
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
 
-let a = () => "aaaa";
-
-console.log( a() );
+let pole = () => {
+  p = (a + b + c) / 2;
+  result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+  return `Pole trójkąta o bokach ${a}, ${b} oraz ${c} wynosi ${result}`;
+};
+console.log(pole());
