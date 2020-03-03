d66d18210229e7f615a713074a9c17c0a236c047
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-03-03 15:31:37 +0100

    display adress info

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -1,15 +1,26 @@
 title = "dr";
 name = "Mateusz";
-surname = 'Kubicki';
-street = 'Długa 15';
-city = 'Kraków';
-zip = '30-781';
-country = 'Poland';
+surname = "Kubicki";
+street = "Długa 15";
+city = "Kraków";
+zip = "30-781";
+country = "Poland";
 
+adress1 =
+  title +
+  " " +
+  surname +
+  "\n" +
+  "ul. " +
+  street +
+  "\n" +
+  zip +
+  " " +
+  city +
+  "\n" +
+  country;
 
-/* 
-dr Mateusz Kubicki
-ul. Długa 15
-30-781 Kraków
-POLAND
-*/
+adress2 = `${title} ${surname} \nul. ${street} \n${zip} ${city} \n${country} `;
+
+console.log(adress1);
+console.log(adress2);
