21ad52b859a9e0bee5b9864daf463efee592cd1e
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-03-10 13:34:44 +0100

    add random array generator

---
+++ 02-JSCoreConcepts/random.js
@@ -0,0 +1,11 @@
+const fillArray = () => {
+  let randNumberArray = [];
+  for (let i = 0; i <= 9; i++) {
+    randNumberArray.push(Math.floor(Math.random() * (20 - 5 + 1)) + 5);
+  }
+  return randNumberArray;
+};
+
+const displayArray = () => {
+  console.log(fillArray());
+};
