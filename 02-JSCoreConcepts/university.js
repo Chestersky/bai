2a60be88094f6275f736094c54bb453420c0cdd6
Tomasz Szkaradek <tomasz.szkrdk@gmail.com>
2020-03-12 00:40:13 +0100

    Add classes

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -1,35 +1,40 @@
-names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
+names = ["Piotr", "Anna", "Michał", "Paulina", "Mateusz", "Magda"];
 
 class Students {
-    constructor(names) {
-        // ...
-    }
+  constructor(names) {
+    this.names = names;
+  }
 
-    startWith(letter){
-        return // names (array) which starts with the letter
-    }
+  startWith(letter) {
+    let arrayWithLetter = this.names.filter(item => {
+      return item.charAt(0) == letter;
+    });
+    return arrayWithLetter;
+  }
 
-    sort() {
-        return // names (array) in alphabetical order 
-    }
+  sort() {
+    return this.names.sort();
+  }
 
-    get() {
-        return // names (array) in natural order
-    }
+  get() {
+    return this.names;
+  }
 
-    getFirst(n) {
-        return // get first 'n' names (array) 
-    }
+  getFirst(n) {
+    return this.names.slice(n);
+  }
 }
 
 students = new Students(names);
 console.log(students.get());
 console.log(students.sort());
-console.log(students.startWith('P'));
-console.log(students.startWith('M').sort());
+console.log(students.startWith("P"));
+console.log(students.startWith("M").sort());
 console.log(students.sort().getFirst(4));
-console.log(students.startWith('M').sort().getFirst(2));
+console.log(
+  students
+    .startWith("M")
+    .sort()
+    .getFirst(2)
+);
 console.log(students.get());
-
-
-
